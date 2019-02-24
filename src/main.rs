cc47cd075be657aea3fdeebf876adc6a91892161
Jordan Osborn <jordanosborn0@gmail.com>
2019-02-24 10:24:32 +0000

    refactored main

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod compiler;
 mod errors;
 #[allow(unused_imports)]
 use compiler::{parser, tokenizer, tokens};
-
+use std::path::Path;
 #[macro_use]
 mod native_interface;
 
@@ -14,18 +14,31 @@ fn parse_args() -> Vec<String> {
 fn main() {
     //TODO: Parallelize multiple files/ Libs to compile
     let args = parse_args();
-    if let Some(filename) = args.get(1) {
-        let contents =
-            std::fs::read_to_string(filename).expect("Something went wrong reading the file");
-        match tokenizer::tokenize(filename, contents) {
-            Ok(t) => {
-                println!("{:?}", t);
-            }
-            Err(e) => {
-                println!("{}", e);
+    match args.get(1) {
+        Some(s) if Path::new(s).is_dir() => {
+            std::env::set_current_dir(Path::new(s)).expect("Can't change to desired directory!");
+            let current_directory =
+                std::env::current_dir().expect("Can't determine current working directory!");
+            println!("{:?}", current_directory);
+            //TODO: check for commands or files repl etc.
+        }
+        Some(command) if Path::new("glisp.toml").is_file() => {
+            println!("Hello {}", { command });
+        }
+        Some(filename) if Path::new(filename).is_file() => {
+            let contents =
+                std::fs::read_to_string(filename).expect("Something went wrong reading the file");
+            match tokenizer::tokenize(filename, contents) {
+                Ok(t) => {
+                    println!("{:?}", t);
+                }
+                Err(e) => {
+                    println!("{}", e);
+                }
             }
         }
-    } else {
-        println!("Incorrect number of arguments supplied!");
+        _ => {
+            println!("Incorrect arguments supplied!");
+        }
     }
 }
